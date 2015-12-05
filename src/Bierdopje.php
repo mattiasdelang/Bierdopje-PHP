068e539fee0ac32c4209e49a17849948df346bdb
Robin Houtevelts <robin@houtevelts.com>
2015-12-06 00:00:07 +0100

    Enforce code style

--- src/Bierdopje.php
+++ src/Bierdopje.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace mattiasdelang;
 
 use Carbon\Carbon;
 use GuzzleHttp\Client;
 
-class Bierdopje
-{
+class Bierdopje {
 
   /**
    * @var string
@@ -20,6 +18,7 @@ class Bierdopje
 
   /**
    * Create Bierdopje API instance
+   *
    * @param Client $client
    */
   public function __construct(Client $client)
@@ -30,16 +29,18 @@ class Bierdopje
 
   /**
    * Search a show by name
+   *
    * @param $showName
+   *
    * @return null|\stdClass
    * @throws \Exception
    */
   public function getShowByName($showName)
   {
     $response = $this->request('/FindShowByName/' . $showName);
-
     $shows = $response->response->results->result;
-    if (count($shows) <= 0)
+
+    if ( count($shows) <= 0 )
       return null;
 
     $show = $shows[0];
@@ -50,35 +51,39 @@ class Bierdopje
 
   /**
    * Search a show by a TVDB showId
+   *
    * @param $tvdbId
+   *
    * @return \stdClass
    * @throws \Exception
    */
   public function getShowByTvdbId($tvdbId)
   {
     $response = $this->request('/GetShowByTVDBID/' . $tvdbId);
-    $show = $this->formatShow($response->response);
+    $show     = $this->formatShow($response->response);
 
     return $show;
   }
 
   /**
    * Search episodes by season and Bierdopje showId
+   *
    * @param $showId
    * @param $season
+   *
    * @return array|null
    * @throws \Exception
    */
   public function getEpisodesOfSeason($showId, $season)
   {
     $response = $this->request('/GetEpisodesForSeason/' . $showId . '/' . $season);
-
     $episodes = $response->response->results->result;
-    if (count($episodes) <= 0)
+
+    if ( count($episodes) <= 0 )
       return null;
 
     $episodeList = [];
-    foreach ($episodes as $episode)
+    foreach ( $episodes as $episode )
       $episodeList[] = $this->formatEpisode($episode);
 
     return $episodeList;
@@ -86,20 +91,22 @@ class Bierdopje
 
   /**
    * Search episodes by a Bierdopje showId
+   *
    * @param $showId
+   *
    * @return array|null
    * @throws \Exception
    */
   public function getEpisodesOfShow($showId)
   {
     $response = $this->request('/GetAllEpisodesForShow/' . $showId);
-
     $episodes = $response->response->results->result;
-    if (count($episodes) <= 0)
+
+    if ( count($episodes) <= 0 )
       return null;
 
     $episodeList = [];
-    foreach ($episodes as $episode)
+    foreach ( $episodes as $episode )
       $episodeList[] = $this->formatEpisode($episode);
 
     return $episodeList;
@@ -107,77 +114,81 @@ class Bierdopje
 
   /**
    * Search an episode by its Bierdopje Id
+   *
    * @param $episodeId
+   *
    * @return \stdClass
    * @throws \Exception
    */
   public function getEpisodeById($episodeId)
   {
     $response = $this->request('/GetEpisodeById/' . $episodeId);
-    $episode = $response->response->results;
-    $episode = $this->formatEpisode($episode);
+    $episode  = $response->response->results;
+    $episode  = $this->formatEpisode($episode);
 
     return $episode;
   }
 
   /**
    * Format an Episode response
+   *
    * @param $original
+   *
    * @return \stdClass
    */
   private function formatEpisode($original)
   {
     $show = new \stdClass();
 
-    $show->id = (int)$original->episodeid;
-    $show->tvdbId = (int)$original->tvdbid;
-    $show->title = (string)$original->title;
-    $show->showlink = (string)$original->showlink;
-    $show->episodelink = (string)$original->episodelink;
-    $show->airDate = Carbon::createFromFormat("d-m-Y", (string)$original->airdate);
-    $show->season = (int)$original->season;
-    $show->episode = (int)$original->episode;
-    $show->epNumber = (int)$original->genres->epnumber;
-    $show->score = (float)str_replace(',', '.', $original->score);
-    $show->votes = (int)$original->votes;
-    $show->formatted = (string)$original->formatted;
-    $show->is_special = ((string)$original->is_special) === "true";
-    $show->summary = (string)$original->summary;
+    $show->id          = (int) $original->episodeid;
+    $show->tvdbId      = (int) $original->tvdbid;
+    $show->title       = (string) $original->title;
+    $show->showlink    = (string) $original->showlink;
+    $show->episodelink = (string) $original->episodelink;
+    $show->airDate     = Carbon::createFromFormat("d-m-Y", (string) $original->airdate);
+    $show->season      = (int) $original->season;
+    $show->episode     = (int) $original->episode;
+    $show->epNumber    = (int) $original->genres->epnumber;
+    $show->score       = (float) str_replace(',', '.', $original->score);
+    $show->votes       = (int) $original->votes;
+    $show->formatted   = (string) $original->formatted;
+    $show->is_special  = ((string) $original->is_special) === "true";
+    $show->summary     = (string) $original->summary;
 
     return $show;
   }
 
   /**
    * Format a Show response
+   *
    * @param $original
+   *
    * @return \stdClass
    */
   private function formatShow($original)
   {
     $show = new \stdClass();
 
-    $show->id = (int)$original->showid;
-    $show->tvdbId = (int)$original->tvdbid;
-    $show->name = (string)$original->showname;
-    $show->link = (string)$original->showlink;
-    $show->firstAired = Carbon::createFromFormat('Y-m-d', (string)$original->firstaired);
-    $show->lastAired = Carbon::createFromFormat('Y-m-d', (string)$original->lastaired);
-    $show->nextEpisode = Carbon::createFromFormat('Y-m-d', (string)$original->nextepisode);
-    $show->seasons = (int)$original->seasons;
-    $show->episodes = (int)$original->episodes;
-    $show->genres = $original->genres->result;
-    $show->score = (float)str_replace(',', '.', $original->score);
-    $show->runtime = (float)str_replace(',', '.', $original->runtime);
-    $show->favorites = (int)$original->favorites;
-    $show->showstatus = (string)$original->showstatus;
-    $show->airtime = (string)$original->airtime;
-    $show->summary = (string)$original->summary;
-
-
+    $show->id          = (int) $original->showid;
+    $show->tvdbId      = (int) $original->tvdbid;
+    $show->name        = (string) $original->showname;
+    $show->link        = (string) $original->showlink;
+    $show->firstAired  = Carbon::createFromFormat('Y-m-d', (string) $original->firstaired);
+    $show->lastAired   = Carbon::createFromFormat('Y-m-d', (string) $original->lastaired);
+    $show->nextEpisode = Carbon::createFromFormat('Y-m-d', (string) $original->nextepisode);
+    $show->seasons     = (int) $original->seasons;
+    $show->episodes    = (int) $original->episodes;
+    $show->genres      = $original->genres->result;
+    $show->score       = (float) str_replace(',', '.', $original->score);
+    $show->runtime     = (float) str_replace(',', '.', $original->runtime);
+    $show->favorites   = (int) $original->favorites;
+    $show->showstatus  = (string) $original->showstatus;
+    $show->airtime     = (string) $original->airtime;
+    $show->summary     = (string) $original->summary;
     $genres = [];
-    foreach ($show->genres as $key => $genre) {
-      $genre = (string)$genre;
-      $genre = ucfirst($genre);
+    foreach ( $show->genres as $key => $genre ) {
+      $genre    = (string) $genre;
+      $genre    = ucfirst($genre);
       $genres[] = $genre;
     }
     $show->genres = $genres;
@@ -187,23 +198,29 @@ class Bierdopje
 
   /**
    * Make an HTTP request and format the XML resposne
+   *
    * @param $path
+   *
    * @return \SimpleXMLElement
    * @throws \Exception
    */
   protected function request($path)
   {
     $response = $this->client->get($this->url . $path);
-    if ($response->getStatusCode() != 200)
+
+    if ( $response->getStatusCode() != 200 )
       throw new \Exception('Bierdopje.com not available');
 
     $response = $this->xmlToObj($response->getBody());
+
     return $response;
   }
 
   /**
    * Convert XML string to object
+   *
    * @param $fileContents
+   *
    * @return \SimpleXMLElement
    */
   private function xmlToObj($fileContents)
--- tests/BierdopjeTest.php
+++ tests/BierdopjeTest.php
@@ -1,9 +1,7 @@
 <?php
-
 use mattiasdelang\Bierdopje as Api;
 
-class Bierdopje extends PHPUnit_Framework_TestCase
-{
+class Bierdopje extends PHPUnit_Framework_TestCase {
 
   /**
    * @var \mattiasdelang\Bierdopje
@@ -18,7 +16,6 @@ class Bierdopje extends PHPUnit_Framework_TestCase
         'User-Agent' => 'mattiasdelang/bierdopje-php/1.0.1'
       ]
     ]);
-
     $bierdopje = new Api($httpClient);
     $this->api = $bierdopje;
   }
@@ -38,11 +35,9 @@ class Bierdopje extends PHPUnit_Framework_TestCase
     #$this->assertEquals($show->nextEpisode->format('d-m-Y'), '27-01-2016');
     $this->assertTrue($show->seasons >= 4, 'There are more than 4 seasons');
     $this->assertTrue($show->episodes >= 93, 'There are more than 93 episodes');
-
     $this->assertTrue(in_array('Action', $show->genres), 'Genres contains "Action"');
     $this->assertTrue(in_array('Adventure', $show->genres), 'Genres contains "Adventure"');
     $this->assertTrue(in_array('Crime', $show->genres), 'Genres contains "Crime"');
-
     $this->assertTrue(strlen($show->summary) > 100, 'The summary has more than 100 chars');
   }
 
@@ -51,7 +46,6 @@ class Bierdopje extends PHPUnit_Framework_TestCase
    */
   public function test_it_fetches_shows_by_tvdbId()
   {
-
     $show = $this->api->getShowBytvdbId(257655);
 
     $this->assertEquals($show->id, 16290);
@@ -62,11 +56,9 @@ class Bierdopje extends PHPUnit_Framework_TestCase
     #$this->assertEquals($show->nextEpisode->format('d-m-Y'), '27-01-2016');
     $this->assertTrue($show->seasons >= 4);
     $this->assertTrue($show->episodes >= 93);
-
     $this->assertTrue(in_array('Action', $show->genres));
     $this->assertTrue(in_array('Adventure', $show->genres));
     $this->assertTrue(in_array('Crime', $show->genres));
-
     $this->assertTrue(strlen($show->summary) > 100);
   }
 
@@ -78,9 +70,7 @@ class Bierdopje extends PHPUnit_Framework_TestCase
     $episodes = $this->api->getEpisodesOfSeason(16290, 2);
 
     $this->assertEquals(count($episodes), 24);
-
     $episode = $episodes[0];
-
     $this->assertEquals($episode->id, 772542);
     $this->assertEquals($episode->tvdbId, 4599381);
     $this->assertEquals($episode->title, "City of Heroes");
@@ -101,7 +91,6 @@ class Bierdopje extends PHPUnit_Framework_TestCase
 
     $this->assertTrue(count($episodes) >= 93);
     $episode = $episodes[0];
-
     $this->assertEquals($episode->id, 814124);
     $this->assertEquals($episode->tvdbId, 4659835);
     $this->assertEquals($episode->title, "Year One");
